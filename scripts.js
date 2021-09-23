6dbe6ee549bd9ef13e799eda52d86708d8e1f0fb
Mehdirps <Mehdi.raposo77@gmail.com>
2021-09-23 10:36:24 +0200

    code afiner

--- scripts.js
+++ scripts.js
@@ -1,31 +1,23 @@
-let nombre = 0;
 let body = document.querySelector("body");
 
-for (let compteur = 1; compteur <= 200; compteur++) {
+for (let compteur = 1; compteur <= 200; compteur++){ 
 
-    let nombre200 = nombre + compteur;
-    let affichage = document.createElement("p")
-    body.appendChild(affichage)
-    
-    let multiple3 = nombre200 % 3;
-    let multiple5 = nombre200 % 5;
-    let multiple15 = nombre200 % 15;
-    
-    if (multiple3 === 0) {
-        
-        nombre200 = "fizz";
-        affichage.style.color = "red"
+    let affichage = document.createElement("p");
+    body.appendChild(affichage);
+    affichage.textContent = compteur;
 
+    if (compteur % 3 === 0) {  
+        affichage.textContent = "fizz";
+        affichage.style.color = "red";
     }
-    if(multiple5 === 0){
+    if(compteur % 5 === 0){
         
-        nombre200 = "Buzz";
-        affichage.style.color = "green"
+        affichage.textContent = "Buzz";
+        affichage.style.color = "green";
     }
-    if(multiple15 === 0){
+    if(compteur % 15 === 0){
         
-        nombre200 = "FizzBuzz";
-        affichage.style.color = "blue"
+        affichage.textContent = "FizzBuzz";
+        affichage.style.color = "blue";
     }
-    affichage.textContent = nombre200;
 }
